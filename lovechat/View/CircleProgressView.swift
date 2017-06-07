cd07d8e14781ac60490b922467573cf554ce5c6b
demons <837940593@qq.com>
2017-06-07 20:48:00 +0800

    ready to add a circle process bar.

---
+++ lovechat/View/CircleProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleProgressView.swift
+//  lovechat
+//
+//  Created by Demons on 2017/6/7.
+//  Copyright © 2017年 Demons. All rights reserved.
+//
+
+import UIKit
+
+class CircleProgressView: UIProgressView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
