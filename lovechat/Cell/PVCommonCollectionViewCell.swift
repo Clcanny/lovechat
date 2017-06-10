6a79703d2465039fb7b23161a88f2c2cf4ba3680
demons <837940593@qq.com>
2017-06-10 21:32:06 +0800

    ready to refactor Picture-Video-MessageCollectionViewCell

---
+++ lovechat/Cell/PVCommonCollectionViewCell.swift
@@ -0,0 +1,168 @@
+//
+//  PVCommonCollectionViewCell.swift
+//  lovechat
+//
+//  Created by Demons on 2017/6/10.
+//  Copyright © 2017年 Demons. All rights reserved.
+//
+
+import UIKit
+
+class PVCommonCollectionViewCell: UICollectionViewCell {
+    
+    public static let maxWidth: CGFloat = 100
+    private static let radius: CGFloat = 15
+    
+    var delegate: SegueFromCellProtocol?
+    private var gesture: UITapGestureRecognizer?
+    func click(gestureRecognizer: UIGestureRecognizer) {
+    }
+    
+    private let avatar = { () -> UIImageView in
+        var imageView = UIImageView()
+        imageView.frame.size = CGSize(width: radius * 2, height: radius * 2)
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = robinEggColor
+        return imageView
+    }()
+    
+    // UIImage contains the data for an image.
+    // UIImageView is a custom view meant to display the UIImage.
+    private let pictureView = { () -> UIImageView in
+        var imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = UIColor.clear
+        return imageView
+    }()
+    public var pictureSize: CGSize {
+        get {
+            return pictureView.frame.size
+        }
+        
+        set(newSize) {
+            pictureView.frame.size = newSize
+        }
+    }
+    
+    private let highlightLayer = { () -> CAShapeLayer in
+        let layer = CAShapeLayer()
+        layer.strokeColor = babyBlueColor.cgColor
+        layer.lineWidth = 1
+        layer.fillColor = babyBlueColor.cgColor
+        return layer
+    }()
+    public func setHightlightColor() {
+        var count = 0
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        let image = pictureView.image!
+        let ys = Int(
+            PVCommonCollectionViewCell.radius
+                * 2.0 / bounds.height * image.size.height
+        )
+        for x in 0..<3 {
+            for y in 0..<ys {
+                count += 1
+                let rgb = image.getPixelColor(
+                    pos: CGPoint(x: CGFloat(x), y: CGFloat(y))
+                )
+                red += rgb.0
+                green += rgb.1
+                blue += rgb.2
+            }
+        }
+        red = red / CGFloat(count * 255)
+        green = green / CGFloat(count * 255)
+        blue = blue / CGFloat(count * 255)
+        self.highlightLayer.fillColor = UIColor(
+            red: red, green: green,
+            blue: blue, alpha: 1).cgColor
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addSubview(avatar)
+        // The order is important.
+        layer.addSublayer(highlightLayer)
+        addSubview(pictureView)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        gesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(PictureMessageCollectionViewCell.click(gestureRecognizer:))
+        )
+//        gesture?.numberOfTapsRequired = 2
+        addGestureRecognizer(gesture!)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func keepLeft() {
+        let radius = PVCommonCollectionViewCell.radius
+        let borderWidth = radius * 3 + pictureView.frame.size.width
+        
+        let path = UIBezierPath()
+        path.addArc(
+            withCenter: CGPoint(x: radius * 2, y: 0),
+            radius: radius,
+            startAngle: .pi / 2,
+            endAngle: 0,
+            clockwise: false
+        )
+        path.addLine(to: CGPoint(x: borderWidth, y: 0))
+        path.addLine(to: CGPoint(x: borderWidth, y: bounds.height))
+        path.addLine(to: CGPoint(x: radius * 3, y: bounds.height))
+        path.addLine(to: CGPoint(x: radius * 3, y: radius * 2))
+        path.addArc(
+            withCenter: CGPoint(x: radius * 2, y: radius * 2),
+            radius: radius,
+            startAngle: 0,
+            endAngle: .pi / 2 * 3,
+            clockwise: false
+        )
+        path.close()
+        
+        highlightLayer.path = path.cgPath
+        
+        avatar.frame.origin = .zero
+        pictureView.frame.origin = CGPoint(x: radius * 3, y: 0)
+    }
+    
+    public func keepRight() {
+        let radius = PVCommonCollectionViewCell.radius
+        let borderX = bounds.width - (radius * 3 + pictureView.frame.size.width)
+        
+        let path = UIBezierPath()
+        path.addArc(
+            withCenter: CGPoint(x: bounds.width - radius * 2, y: 0),
+            radius: radius,
+            startAngle: .pi,
+            endAngle: .pi / 2,
+            clockwise: false
+        )
+        path.addArc(
+            withCenter: CGPoint(x: bounds.width - radius * 2, y: radius * 2),
+            radius: radius,
+            startAngle: .pi / 2 * 3,
+            endAngle: .pi,
+            clockwise: false
+        )
+        path.addLine(to: CGPoint(x: bounds.width - radius * 3, y: bounds.height))
+        path.addLine(to: CGPoint(x: borderX, y: bounds.height))
+        path.addLine(to: CGPoint(x: borderX, y: 0))
+        path.addLine(to: CGPoint(x: bounds.width - radius * 3, y: 0))
+        path.close()
+        
+        highlightLayer.path = path.cgPath
+        
+        avatar.frame.origin = CGPoint(x: bounds.width - radius * 2, y: 0)
+        pictureView.frame.origin = CGPoint(x: borderX, y: 0)
+    }
+    
+}
