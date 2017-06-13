e0ed08ee910ba4da974dd8c3e5c46474330912cd
demons <837940593@qq.com>
2017-06-13 13:50:27 +0800

    It's not the code problem, please use toggle software keyboard to make keyboard appear.

---
+++ lovechat/Controller/TextKeyboardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TextKeyboardViewController.swift
+//  lovechat
+//
+//  Created by Demons on 2017/6/13.
+//  Copyright © 2017年 Demons. All rights reserved.
+//
+
+import UIKit
+
+class TextKeyboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
